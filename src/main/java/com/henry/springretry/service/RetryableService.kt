a6fe16bee30a3a196b4425412eb71584213eaf13
henry <henry@lendit.co.kr>
2020-02-04 16:19:14 +0900

    fixed back off -> exponential back off

--- src/main/java/com/henry/springretry/service/RetryableService.kt
+++ src/main/java/com/henry/springretry/service/RetryableService.kt
@@ -1,7 +1,7 @@
 package com.henry.springretry.service
 
 import mu.KLogging
-import org.springframework.retry.backoff.FixedBackOffPolicy
+import org.springframework.retry.backoff.ExponentialBackOffPolicy
 import org.springframework.retry.policy.SimpleRetryPolicy
 import org.springframework.retry.support.RetryTemplate
 import org.springframework.stereotype.Service
@@ -15,13 +15,17 @@ class RetryableService {
         action: () -> Unit,
         maxAttempts: Int = 3,
         exceptions: List<Class<out Throwable>> = listOf(Exception::class.java),
-        backOff: Long = 2000L
+        maxInterval: Long = 2000L
     ) {
         val retryTemplate = RetryTemplate()
 
-        val fixedBackOffPolicy = FixedBackOffPolicy()
-        fixedBackOffPolicy.backOffPeriod = backOff
-        retryTemplate.setBackOffPolicy(fixedBackOffPolicy)
+//        val fixedBackOffPolicy = FixedBackOffPolicy() // fixed back off
+        val exponentialBackOffPolicy = ExponentialBackOffPolicy().apply {
+            this.initialInterval = 300L
+            this.maxInterval = maxInterval
+            this.multiplier = 2.0
+        } // exponential back off
+        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy)
 
         val exceptionMap = exceptions.map { it to true }.toMap()
         val retryPolicy = SimpleRetryPolicy(maxAttempts, exceptionMap)
