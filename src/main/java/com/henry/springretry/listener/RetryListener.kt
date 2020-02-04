dcd816d2e8c6d27055ac7f125530aaed6f151d9a
henry <henry@lendit.co.kr>
2020-02-04 18:34:40 +0900

    add stacktrace log

--- src/main/java/com/henry/springretry/listener/RetryListener.kt
+++ src/main/java/com/henry/springretry/listener/RetryListener.kt
@@ -12,6 +12,7 @@ class RetryListener: RetryListenerSupport() {
     override fun <T : Any?, E : Throwable?> onError(context: RetryContext?, callback: RetryCallback<T, E>?, throwable: Throwable?) {
         if (context?.retryCount == 1) return // onError 리스너이기 때문에 오류가 나고 재시도하기 전에 불림
 
+        logger.info("stacktrace: ${context?.lastThrowable?.stackTrace?.get(0)}")
         logger.info("error retry! retry cnt: ${context?.retryCount?.minus(1)} exception: $throwable")
         super.onError(context, callback, throwable)
     }
